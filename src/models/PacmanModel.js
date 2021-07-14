7b500a1e673ad37a59e19934eecd7b1cafebf341
Ehsan Gazar <me@ehsangazar.com>
2021-07-14 15:50:00 +1000

    added pacman report

--- src/models/PacmanModel.js
+++ src/models/PacmanModel.js
@@ -78,6 +78,7 @@ class PacmanModel {
   report() {
     if (!this.isPlaced) return false
     const state = { x: this.x, y: this.y, face: this.face }
+    console.log('REPORT', state)
     return state
   }
 
